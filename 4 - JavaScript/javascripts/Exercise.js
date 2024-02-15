ca20aeb0ed8cf3cf37bcc77284a242a6bd220605
Steven Sousa <silver.steven67@gmail.com>
2024-02-15 11:37:42 -0500

    Finished JavaScript exercise

--- 4 - JavaScript/javascripts/Exercise.js
+++ 4 - JavaScript/javascripts/Exercise.js
@@ -1,15 +1,23 @@
 function checkPassword() {
+    console.log("Im here");
+    let text = document.getElementById("text");
     const password1 = document.getElementById("password1");
     const password2 = document.getElementById("password2");
-    if (!password1.value.test(password2.value)) {
-        document.getElementsByTagName("p").innerHTML = "Passwords do not match";
-    }
-    else
-        document.getElementsByTagName("p").innerHTML = "Passwords match";
-}
 
+    if (password1.value == "" || password2.value == "") {
+        text.innerText = "You did not enter a password. Enter one now.";
+        text.style.color = "blue";
+    }
+    else if (password1.value != password2.value) {
+        text.innerText = "The two passwords you entered are not the same. Try again.";
+        text.style.color = "red";
+    }
+    else{
+        text.innerText = "Hooray! You have matched passwords!";
+        text.style.color = "green";
+    }
+}
 // Register listeners
-window.addEventListener('load', ()=>{
-    document.getElementById("password1").addEventListener("change", password1);
-    document.getElementById("password2").addEventListener("change", password2);
+window.addEventListener('load', () => {
+    document.getElementById("button").addEventListener("click", checkPassword);
 })
